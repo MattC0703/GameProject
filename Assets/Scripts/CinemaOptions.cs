b1c0d2cd6d1025a317e959fa16e43b8c2dcf0464
MattC0703 <matthewjc0703@gmail.com>
2025-04-09 02:22:58 -0400

    updated camera logic

--- Assets/Scripts/CinemaOptions.cs
+++ Assets/Scripts/CinemaOptions.cs
@@ -9,6 +9,7 @@ public class CinemaOptions : MonoBehaviour
 {
     public CinemachineCamera vCam;
     public CinemachineCamera freeCam;
+    public CinemachineThirdPersonFollow vCamFollow;
 
     public Transform playerTransform;
 
@@ -17,18 +18,25 @@ public class CinemaOptions : MonoBehaviour
     public float transTime = .3f;
 
     public bool isFreeCam = false;
+
+    public float zDeviation = 0f;
+    public float initialZ;
+    public Vector3 initialCamOffset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         vCam.Priority = 10;
         freeCam.Priority = 5;
-      
+        initialZ = playerTransform.eulerAngles.z;
+        
+        initialCamOffset = vCamFollow.ShoulderOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
         CamSwitch();
+        CamAdjust();
     }
 
     void CamSwitch(){
@@ -46,5 +54,18 @@ public class CinemaOptions : MonoBehaviour
             }        
             
         }
+    }
+
+    //adjusts the camera when the player is rotating to retain visual clarity at different angles
+    void CamAdjust(){
+        float currentZ = playerTransform.eulerAngles.z;
+
+        float deviation = Mathf.DeltaAngle(initialZ, currentZ);
+        
+        if(Mathf.Abs(deviation)< .05) deviation = 0;
+
+        zDeviation = Mathf.Max(0f, deviation);
+
+        vCamFollow.ShoulderOffset = new Vector3(initialCamOffset.x-zDeviation, initialCamOffset.y-zDeviation/6, vCamFollow.ShoulderOffset.z);
     }
 }
